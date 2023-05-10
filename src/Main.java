afc9d4dedeb558bfd225f21fdc6ce4e946a482ec
BT_4N2_22 <yhuang6817@bths.edu>
2023-05-10 12:22:33 -0400

    Made a GUI frame and finished adding background, game icon, text label

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,31 @@ import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
-        JFrame frame = new JFrame(); //creates a frame
+        /*JFrame frame = new JFrame(); //creates a frame
         frame.setTitle("Title goes here"); //sets the title of the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exits the frame when close
         frame.setSize(800, 650); //sets the x and y dimensions of the frame
         frame.setResizable(false); //prevent frame from resize with mouse
         frame.setVisible(true); //makes the frame visible
         JLabel label = new JLabel("I code"); //creates a label and set text, could also use the setText method
-        frame.add(label); //
-        ImageIcon image1 = new ImageIcon("img.png");
+        frame.add(label);
+        ImageIcon image1 = new ImageIcon("galaxy.jpg");
         label.setIcon(image1);
         label.setHorizontalTextPosition(JLabel.CENTER);
 
-        frame.getContentPane().setBackground(new Color(0x0fa8e1)); //change background color
+        //frame.getContentPane().setBackground(); //change background color
 
-        ImageIcon image = new ImageIcon("Image1.png"); //creates an image icon
-        frame.setIconImage(image.getImage()); //change icon of frame
+        ImageIcon image = new ImageIcon("Image1.jfif"); //creates an image icon
+        frame.setIconImage(image.getImage()); //change icon of frame*/
 
-
+        MyFrame frame = new MyFrame();
+        //frame.getContentPane().add(new JPanelWithBackground("Pictures/galaxy.png"));
 
 
 
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,19 +1,46 @@
 import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 import java.awt.*;
-import java.awt.Color;
 
 
 public class MyFrame extends JFrame {
 
     MyFrame(){
-        this.setTitle("Title goes here"); //sets the title of the frame
+
+        JLabel label = new JLabel(); //create a label
+
+        ImageIcon icon = new ImageIcon("Pictures/Icon.png"); //creates an image icon
+        this.setIconImage(icon.getImage()); //change icon of frame
+
+        ImageIcon image = new ImageIcon("Pictures/galaxy.png"); //creates image of galaxy
+        label.setIcon(image); //show image of galaxy
+
+        //manipulates the label
+        label.setText("Welcome to CardMatching Game!"); //set text of label
+        label.setHorizontalTextPosition(JLabel.CENTER); //set horizontal position of text: RIGHT, CENTER, LEFT
+        label.setVerticalTextPosition(JLabel.TOP); //set vertical position of text: TOP, CENTER, BOTTOM
+        label.setForeground(Color.WHITE); //set color of text
+        label.setFont(new Font("MV Boli", Font.PLAIN, 30)); //set font and size of text
+        label.setIconTextGap(-200); //makes the text move up
+
+
+
+        this.setTitle("A Test of Memory!"); //sets the title of the frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exits the frame when close
-        this.setSize(800, 650); //sets the x and y dimensions of the frame
+        this.setSize(975, 657); //sets the x and y dimensions of the frame
+        this.setLocation(450, 150); //set location of frame
         this.setResizable(false); //prevent frame from resize with mouse
+        this.add(label); //add label to frame
+
+
         this.setVisible(true); //makes the frame visible
-        ImageIcon image = new ImageIcon("Image1.jfif"); //creates an image icon
-        this.setIconImage(image.getImage()); //change icon of frame
-        this.getContentPane().setBackground(new Color(0x00a8e1)); //change background color
+
+
+
+
+
+
 
 
     }
