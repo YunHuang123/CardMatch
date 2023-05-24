fa332c89402e5bc46a1c4e53c6cd6905fe064b91
YunHuang123 <114007330+YunHuang123@users.noreply.github.com>
2023-05-24 19:41:05 -0400

    Objects for all three levels created, set up each frame

--- src/Level3.java
+++ src/Level3.java
@@ -1,19 +1,22 @@
 import javax.swing.*;
 
-public class Level2 extends JFrame{
+public class Level3 extends JFrame{
     private ImageIcon cardBack;
-
     private ImageIcon sun;
     private ImageIcon moon;
     private ImageIcon mercury;
     private ImageIcon venus;
     private ImageIcon earth;
-
     private ImageIcon mars;
     private ImageIcon jupiter;
     private ImageIcon saturn;
     private ImageIcon uranus;
     private ImageIcon neptune;
+    private ImageIcon pluto;
+    private ImageIcon comets;
+    private ImageIcon asteroids;
+    private ImageIcon meteorite;
+    private ImageIcon telescope;
     private JLabel label1;
     private JLabel label2;
     private JLabel label3;
@@ -34,21 +37,34 @@ public class Level2 extends JFrame{
     private JLabel label18;
     private JLabel label19;
     private JLabel label20;
+    private JLabel label21;
+    private JLabel label22;
+    private JLabel label23;
+    private JLabel label24;
+    private JLabel label25;
+    private JLabel label26;
+    private JLabel label27;
+    private JLabel label28;
+    private JLabel label29;
+    private JLabel label30;
 
-    public Level2(){
+    public Level3(){
         cardBack = new ImageIcon("Pictures/cardBack.png");
         sun = new ImageIcon("Pictures/Sun.png");
         moon = new ImageIcon("Pictures/Moon.png");
         mercury = new ImageIcon("Pictures/Mercury.png");
         venus = new ImageIcon("Pictures/Venus.png");
         earth = new ImageIcon("Pictures/Earth.png");
+        mars = new ImageIcon("Pictures/Mars.png");
         jupiter = new ImageIcon("Pictures/Jupiter.png");
         saturn = new ImageIcon("Pictures/Saturn.png");
         uranus = new ImageIcon("Pictures/Uranus.png");
         neptune = new ImageIcon("Pictures/Neptune.png");
-
-
-
+        pluto = new ImageIcon("Pictures/Pluto.png");
+        comets = new ImageIcon("Pictures/Comets.png");
+        asteroids = new ImageIcon("Pictures/Asteroids.png");
+        meteorite = new ImageIcon("Pictures/Meteorite.png");
+        telescope = new ImageIcon("Pictures/Telescope.png");
 
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
