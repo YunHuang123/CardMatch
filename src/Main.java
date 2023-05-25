09a6438a6872e0f0d5c6fe8f99aac2713ffe8ecd
BT_4N2_22 <yhuang6817@bths.edu>
2023-05-25 12:20:27 -0400

    Finished setting up all the frames, next work on the actual game itself. next step: work on transferring from frame to frame

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        //HomeFrame frame = new HomeFrame();
 
         //new HomePage();
         //new Level1();
         //new Level2();
-        new Level3();
+        //new Level3();
 
 
 
